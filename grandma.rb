8003f9302116cf7f7dd110801fa3af1c86509ad7
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-01 20:43:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string = "I LOVE YOU GRANDMA")
-  if string = string.uppercase
+  if string = "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY!"
   else string = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
