9c9fcf97dcb13bc81b746f2ee5aaaa03387e73d9
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-01 20:42:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -15,10 +15,7 @@
 def speak_to_grandma(string = "I LOVE YOU GRANDMA")
   if string = string.uppercase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif string = 
   else string = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
-
-    
