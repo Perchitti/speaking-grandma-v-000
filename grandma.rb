f3a29db94dbe3f834edf3b594ec76d659d1245b0
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-01 21:09:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -19,6 +19,6 @@ def speak_to_grandma(phrase)
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
-    return "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   end
 end
