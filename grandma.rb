deba86e0fb0f4ca7725398a22d119947b5a506ed
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-01 21:08:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase() || phrase == phrase.downcase()
+  if phrase != phrase.upcase()
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
