815dcb34bc59a7ad94b786e733d36e2ac6798708
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-01 20:52:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(string = "HUH?! SPEAK UP, SONNY!")
   if string = "Hi Nana, how are you?" || "Hi"
-    puts string
+   string
   elsif string = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
